844240144b16599ba1a442f5e3f33ab62e27d4d7
Gowthami <gowthami5024@gmail.com>
2020-11-08 14:31:52 -0500

    Added Entity classes
    Added Entity classes

---
+++ src/main/java/com/hcl/emobileconnect/model/Status.java
@@ -0,0 +1,11 @@
+package com.hcl.emobileconnect.model;
+
+public enum Status {
+	
+	In_Progress, 
+	Approved,
+	Rejected, 
+	Refer_Back, 
+	Connection_Enabled
+
+}
--- src/main/java/com/hcl/emobileconnect/model/User.java
+++ src/main/java/com/hcl/emobileconnect/model/User.java
@@ -1,5 +1,59 @@
 package com.hcl.emobileconnect.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class User {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email_id")
+	private String emailId;
+	
+	@Column(name = "plan")
+	private String plan;
+	
+	@Column(name = "ref_phone_no")
+	private String refPhoneNo;
+	
+	@Column(name = "status")
+	private Status status;
+	
+	@Column(name = "new_phone")
+	private String newPhoneNo;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name = "doc_id"), name = "doc_idid")
+    private UserDocDetails documents;	
+	
 }
