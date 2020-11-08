28601e23931d7b0b4cd5c125d91cc9e34b97e1e1
Niru6241 <niru18.gitam@gmail.com>
2020-11-08 13:47:46 -0600

    POST end point

--- src/main/java/com/hcl/emobileconnect/controller/MobileController.java
+++ src/main/java/com/hcl/emobileconnect/controller/MobileController.java
@@ -2,13 +2,15 @@ package com.hcl.emobileconnect.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.hcl.emobileconnect.dao.UserDao;
 import com.hcl.emobileconnect.model.User;
 import com.hcl.emobileconnect.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
@@ -21,6 +23,16 @@ public class MobileController {
 	
 	@Autowired
 	UserService userService;
+
+	@Autowired
+	UserDao userDao;
+
+
+	@PostMapping(path = "/users", consumes = "application/json", produces = "application/json")
+	public User createUser(@RequestBody User user) {
+		return userDao.save(user);
+	}
+
 	
 	@GetMapping("/user")
 	public List<User> list() {
