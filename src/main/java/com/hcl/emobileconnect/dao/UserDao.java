566f6e696e2349ca11e9923c2a2e8513db8cd71b
Niru6241 <niru18.gitam@gmail.com>
2020-11-08 14:28:02 -0600

    Removed DAO class

--- src/main/java/com/hcl/emobileconnect/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.hcl.emobileconnect.dao;
-
-import com.hcl.emobileconnect.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-}
